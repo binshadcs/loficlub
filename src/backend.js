98c5c333f0d76bb8940a0b4e2501ff8fa1d69564
saviomartin <saviomartin2007@gmail.com>
2021-05-10 13:56:41 +0530

    add firebase count check

--- src/backend.js
+++ src/backend.js
@@ -15,4 +15,33 @@ firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 
+var userRef = firebase.database().ref("users");
+
+firebase
+  .database()
+  .ref(".info/connected")
+  .on("value", function (snapshot) {
+    // If we're not currently connected, don't do anything.
+    if (snapshot.val() == false) {
+      return;
+    }
+
+    // If we are currently connected, then use the 'onDisconnect()'
+    // method to add a set which will only trigger once this
+    // client has disconnected by closing the app,
+    // losing internet, or any other means.
+    userRef
+      .onDisconnect()
+      .update(userRef.value() - 1)
+      .then(function () {
+        // The promise returned from .onDisconnect().set() will
+        // resolve as soon as the server acknowledges the onDisconnect()
+        // request, NOT once we've actually disconnected:
+
+        // We can now safely set ourselves as 'online' knowing that the
+        // server will mark us as offline once we lose connection.
+        userRef.update(userRef.value() + 1);
+      });
+  });
+
 export { db, auth };
