e1ffea0034ee8df5fe3a4e261b19e857c506cb0c
saviomartin <saviomartin2007@gmail.com>
2021-05-10 11:14:49 +0530

    initial

--- src/core/Pomodoro.js
+++ src/core/Pomodoro.js
@@ -7,7 +7,7 @@ import { Button } from "@material-ui/core";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
 // icons
-import { BsClockHistory } from "react-icons/bs";
+import { BsClockHistory, BsDash, BsPlus } from "react-icons/bs";
 import { IoPauseCircleOutline } from "react-icons/io5";
 
 const Pomodoro = () => {
@@ -34,88 +34,126 @@ const Pomodoro = () => {
   };
 
   return (
-    <div className="flex items-center justify-center flex-col h-full w-full pb-[10%]">
-      <div className="m-6">
-        <CountdownCircleTimer
-          isPlaying={isPlaying}
-          key={key}
-          duration={
-            isWorkTime ? convertToSec(worktime) : convertToSec(breaktime)
-          }
-          colors={[
-            ["#3a49d4", 0.33],
-            ["#3a49d4", 0.33],
-            ["#3a49d4", 0.33],
-          ]}
-          size={220}
-          onComplete={() => {
-            setIsWorkTime(!isWorkTime);
-            setKey((prevKey) => prevKey + 1);
-            return [true, 1000];
-          }}
-        >
-          {({ remainingTime }) => {
-            return <h3 className="text-5xl">{convertToMin(remainingTime)}</h3>;
-          }}
-        </CountdownCircleTimer>
-      </div>
-
-      {!isWorkTime && (
-        <h1 className="text-4xl font-medium mb-3">
-          Break Time, New Session Start in {breaktime} mins
-        </h1>
-      )}
-      <div className="flex">
-        <div
-          className="border border-[#4CD2D6] rounded-md m-1 hover:bg-[#4CD2D680] duration-500"
-          onClick={() => {
-            setTimes({
-              worktime: worktime + 5,
-            });
-            setKey((prevKey) => prevKey + 1);
-          }}
-        >
-          <Button className="track flex text-xl">
-            <span className="text-xl font-light">+ 5 Work Time</span>
-          </Button>
+    <div className="relative flex items-center justify-center h-full w-full pb-[10%]">
+      <div className="w-[70%] h-full flex items-center justify-center flex-col">
+        <div className="m-6">
+          <CountdownCircleTimer
+            isPlaying={isPlaying}
+            key={key}
+            duration={
+              isWorkTime ? convertToSec(worktime) : convertToSec(breaktime)
+            }
+            colors={[
+              ["#3a49d4", 0.33],
+              ["#3a49d4", 0.33],
+              ["#3a49d4", 0.33],
+            ]}
+            size={220}
+            onComplete={() => {
+              setIsWorkTime(!isWorkTime);
+              setKey((prevKey) => prevKey + 1);
+              return [true, 1000];
+            }}
+          >
+            {({ remainingTime }) => {
+              return (
+                <h3 className="text-5xl">{convertToMin(remainingTime)}</h3>
+              );
+            }}
+          </CountdownCircleTimer>
         </div>
-        <div
-          className="border border-[#4CD2D6] rounded-md m-1 hover:bg-[#4CD2D680] duration-500"
-          onClick={() => {
-            if (!isWorkTime) {
+
+        {!isWorkTime && (
+          <h1 className="text-4xl font-medium mb-3">
+            Break Time, New Session Start in {breaktime} mins
+          </h1>
+        )}
+        <div className="flex">
+          <div
+            className="border border-[#4CD2D6] rounded-md m-1 hover:bg-[#4CD2D680] duration-500"
+            onClick={() => {
               setTimes({
-                breaktime: breaktime + 5,
+                worktime: worktime + 5,
               });
               setKey((prevKey) => prevKey + 1);
-            }
-          }}
-        >
-          <Button className="track flex text-xl">
-            <span className="text-xl font-light">+ 5 Break Time</span>
-          </Button>
+            }}
+          >
+            <Button className="track flex text-xl">
+              <span className="text-xl font-light">+ 5 Work Time</span>
+            </Button>
+          </div>
+          <div
+            className="border border-[#4CD2D6] rounded-md m-1 hover:bg-[#4CD2D680] duration-500"
+            onClick={() => {
+              if (!isWorkTime) {
+                setTimes({
+                  breaktime: breaktime + 5,
+                });
+                setKey((prevKey) => prevKey + 1);
+              }
+            }}
+          >
+            <Button className="track flex text-xl">
+              <span className="text-xl font-light">+ 5 Break Time</span>
+            </Button>
+          </div>
+          <div
+            className="border border-[#4CD2D6] rounded-md m-1 hover:bg-[#4CD2D680] duration-500"
+            onClick={() => {
+              setKey((prevKey) => prevKey + 1);
+            }}
+          >
+            <Button className="track flex text-xl">
+              <span className="text-xl font-light flex items-center justify-center">
+                Restart <BsClockHistory className="ml-1" />
+              </span>
+            </Button>
+          </div>
+          <div
+            className="border border-[#4CD2D6] rounded-md m-1 hover:bg-[#4CD2D680] duration-500"
+            onClick={() => setIsPlaying(!isPlaying)}
+          >
+            <Button className="track flex text-xl">
+              <span className="text-xl font-light flex items-center justify-center">
+                Pause
+                <IoPauseCircleOutline className="text-2xl ml-2" />
+              </span>
+            </Button>
+          </div>
         </div>
-        <div
-          className="border border-[#4CD2D6] rounded-md m-1 hover:bg-[#4CD2D680] duration-500"
-          onClick={() => {
-            setKey((prevKey) => prevKey + 1);
-          }}
-        >
-          <Button className="track flex text-xl">
-            <span className="text-xl font-light flex items-center justify-center">
-              Restart <BsClockHistory className="ml-1" />
-            </span>
-          </Button>
-        </div>
-        <div
-          className="border border-[#4CD2D6] rounded-md m-1 hover:bg-[#4CD2D680] duration-500"
-          onClick={() => setIsPlaying(!isPlaying)}
-        >
-          <Button className="track flex text-xl">
-            <span className="text-xl font-light flex items-center justify-center">
-              Pause
-              <IoPauseCircleOutline className="text-2xl ml-2" />
-            </span>
-          </Button>
+      </div>
+      <div className="w-[30%] h-full bg-gray-50 mr-5 rounded-md bg-gradient-to-t from-[#000] to-[#333] border border-[#4CD2D6] flex items-center justify-center">
+        <div className="flex items-center justify-center flex-col">
+          <h1 className="text-4xl leading-8">{worktime} Mins</h1>
+          <h1 className="text-md text-gray-300">Session Time</h1>
+          <div className="flex items-center justify-center mt-1">
+            <div className="flex items-center overflow-hidden border border-[#4CD2D6] rounded-md ml-1 mr-1">
+              <Button className="">
+                <BsPlus className=" text-2xl text-[#F0E9E2]" />
+              </Button>
+            </div>
+            <div className="flex items-center overflow-hidden border border-[#4CD2D6] rounded-md ml-1 mr-1">
+              <Button className="">
+                <BsDash className=" text-2xl text-[#F0E9E2]" />
+              </Button>
+            </div>
+          </div>
+          <div className="flex items-center justify-center flex-col mt-6">
+            <h1 className="text-4xl leading-8">{breaktime} Mins</h1>
+            <h1 className="text-md text-gray-300">Break Time</h1>
+            <div className="flex items-center justify-center mt-1">
+              <div className="flex items-center overflow-hidden border border-[#4CD2D6] rounded-md ml-1 mr-1">
+                <Button className="">
+                  <BsPlus className=" text-2xl text-[#F0E9E2]" />
+                </Button>
+              </div>
+              <div className="flex items-center overflow-hidden border border-[#4CD2D6] rounded-md ml-1 mr-1">
+                <Button className="">
+                  <BsDash className=" text-2xl text-[#F0E9E2]" />
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
