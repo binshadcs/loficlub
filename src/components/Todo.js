06d5332bba9be5d29076d98e0ca58b5920a11074
saviomartin <saviomartin2007@gmail.com>
2021-05-12 18:59:40 +0530

    made todo responsive

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,19 +4,19 @@ import { BsCheckBox, BsSquare, BsTrash } from "react-icons/bs";
 
 const Todo = ({ todo, deleteTodo, markAsCompleted }) => {
   return (
-    <div className="w-[68%] relative p-3 flex items-center justify-between bg-gradient-to-l from-[#000] to-[#11111198] border border-[#4CD2D690] rounded-md mt-2 animate__animated animate__fadeInUp">
+    <div className="w-[90%] lg:w-[68%] relative p-3 flex items-center justify-between bg-gradient-to-l from-[#000] to-[#11111198] border border-[#4CD2D690] rounded-md mt-2 animate__animated animate__fadeInUp">
       <div className="relative flex items-center justify-start">
         <div
           className={`absolute duration-500 bg-green-400 h-1 ${
             todo.completed ? "w-full" : "w-[0px]"
           }`}
         ></div>
-        <h2 className="text-2xl font-medium">{todo.name}</h2>
+        <h2 className="text-xl lg:text-2xl font-medium">{todo.name}</h2>
       </div>
       <div className="flex">
         <Button className="track flex" onClick={() => markAsCompleted(todo.id)}>
           <Tooltip title={todo.completed ? "Completed" : "Not Completed"} arrow>
-            <div className="flex items-center justify-center text-xl text-green-600 hover:text-green-400 duration-300">
+            <div className="flex items-center justify-center text-lg lg:text-xl text-green-600 hover:text-green-400 duration-300">
               Completed
               {todo.completed ? (
                 <BsCheckBox className="ml-1" />
@@ -28,7 +28,7 @@ const Todo = ({ todo, deleteTodo, markAsCompleted }) => {
         </Button>
         <Button className="track flex" onClick={() => deleteTodo(todo.id)}>
           <Tooltip title="Delete" arrow>
-            <div className="flex items-center justify-center text-xl text-red-500 hover:text-red-600 duration-300">
+            <div className="flex items-center justify-center text-lg lg:text-xl text-red-500 hover:text-red-600 duration-300">
               Delete
               <BsTrash className="ml-1" />
             </div>
